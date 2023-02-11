a0efc70c8ff9b3571915607b4826ae21e29b6359
Lalita Kumari <lalitasng1534@gmail.com>
2023-02-11 23:43:18 +0530

    made edit delete functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [toDo,setToDo] = useState('');
+  const [toDos,setToDos] = useState(['to wake up','to brush','to make food']);
+
+  const handleAddToDo = () => {
+    if(toDo){
+      setToDos(prevToDos=>{
+        return(
+          [...prevToDos,toDo]
+        )
+      })
+    }
+    setToDo(" ")
+  }
+
+  const handleEdit = (index) =>{
+    console.log('index',index);
+    toDos.find((toDo,i)=>{
+      if(index==i){
+        setToDo(toDo)
+      }
+    })
+    handleDelete(index)
+  }
+
+  
+  const handleDelete = (index) => {
+    setToDos((prevToDos)=>{
+      return(
+        prevToDos.filter((toDo,i)=>i!=index)
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' placeholder='Add a new task' value = {toDo} onChange={(e)=>setToDo(e.target.value)}/>
+      <button onClick={handleAddToDo}>Add</button>
+      <div>
+        <ol>
+          {
+            toDos.map((toDo,index) => <li key={`${toDo}-${index}`}>{toDo}<span><button onClick={()=>handleEdit(index)}>Edit</button><button onClick={()=>handleDelete(index)}>Delete</button></span></li>)
+          }
+        </ol>
+      </div>
     </div>
   );
 }
